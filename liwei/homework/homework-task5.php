cf3648b0b969e5bb341806effb0e7b9be642680b
tlweiii6465 <tlweiii646564@gmail.com>
2017-09-23 15:19:03 +0800

    siap

--- liwei/homework/homework-task5.php
+++ liwei/homework/homework-task5.php
@@ -8,6 +8,8 @@ $x2 = readline('insert position x2 = ');
 
 $y2 = readline('insert position y2 = ');
 
-echo "the distance of coordinate is " .(sqrt(pow(($x2 - $x1), 2) + pow(($y2 - $y1), 2))) . PHP_EOL;
+$coordinate = (sqrt(pow(($x2 - $x1), 2) + pow(($y2 - $y1), 2)));
+
+echo "the distance between each other is " . number_format((float)$coordinate, 2, '.', '') . PHP_EOL; 
 
 ?>
