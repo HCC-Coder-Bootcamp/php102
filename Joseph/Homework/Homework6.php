0f11cf5dc884e4a97376704c49ed7499d3c0e933
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-09-24 09:10:26 +0800

    something wrong

--- Joseph/Homework/Homework6.php
+++ Joseph/Homework/Homework6.php
@@ -2,13 +2,13 @@
 
 $inputNumber = (int)readline('Please enter 5 digits number.' . PHP_EOL);
 
-$tenThousand = ($inputNumber%10)*10000;
-$thousand     = (($inputNumber/10)%10)*1000;
-$hundred      = (($inputNumber/100)%10)*100;
-$tens         = (($inputNumber/1000)%10)*10;
-$one          = ($inputNumber/10000)%10;
+$tenThousand  = ($inputNumber % 10) * 10000;
+$thousand     = (($inputNumber / 10) % 10) * 1000;
+$hundred      = (($inputNumber / 100) % 10) * 100;
+$tens         = (($inputNumber /1000) % 10) * 10;
+$one          = ($inputNumber / 10000) % 10;
 
-$answer = $ten_thousand + $thousand + $hundred + $tens + $one;
+$answer = $tenThousand + $thousand + $hundred + $tens + $one;
 
 echo "Here is your answer " . (string)$answer . "." . PHP_EOL;
 
