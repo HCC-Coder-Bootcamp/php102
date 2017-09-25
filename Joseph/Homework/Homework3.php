ae4c034de6fdf08ebb3b8c96bd7fac9c87c77248
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-09-25 13:32:54 +0800

    ...

--- Joseph/Homework/Homework3.php
+++ Joseph/Homework/Homework3.php
@@ -2,11 +2,9 @@
 
 $inputRadius = (float)readline('Please enter the radius of the circle.' . PHP_EOL);
 
-$π = (float)3.14;
+$perimeter = 2 * (M_PI * $inputRadius);
+$theArea   = M_PI * ($inputRadius * $inputRadius);
 
-$perimeter = 2 * ($π * $inputRadius);
-$theArea   = $π * ($inputRadius * $inputRadius);
-
-echo "The perimeter of the circle is " . (string)number_format((float)$perimeter, 2, '.', '') . "cm ." .  PHP_EOL . "And the area of the circle is " . (string)number_format((float)$theArea, 2, '.', '') . "cm^2 ." . PHP_EOL;
+echo "The perimeter of the circle is " . (string)number_format((float)$perimeter, 2, '.', ',') . "cm ." .  PHP_EOL . "And the area of the circle is " . (string)number_format((float)$theArea, 2, '.', ',') . "cm^2 ." . PHP_EOL;
 
  ?>
--- Joseph/Homework/Homework6.php
+++ Joseph/Homework/Homework6.php
@@ -5,7 +5,7 @@ $inputNumber = (int)readline('Please enter 5 digits number.' . PHP_EOL);
 $tenThousand  = ($inputNumber % 10) * 10000;
 $thousand     = (($inputNumber / 10) % 10) * 1000;
 $hundred      = (($inputNumber / 100) % 10) * 100;
-$tens         = (($inputNumber /1000) % 10) * 10;
+$tens         = (($inputNumber / 1000) % 10) * 10;
 $one          = ($inputNumber / 10000) % 10;
 
 $answer = $tenThousand + $thousand + $hundred + $tens + $one;
